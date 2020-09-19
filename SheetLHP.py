82db0cde5df6bd82653556d4e33c3f82f230cd13
manhhung99 <52267054+manhhung99@users.noreply.github.com>
2020-09-19 21:32:28 +0700

    Add files via upload

---
+++ SheetLHP.py
@@ -0,0 +1,25 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("/home/manhhung060299/CoTAI/face_recognition/FaceRecLHP/ConnectGgSheet/Sheetlhp.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("LHP").sheet1  # Open the spreadhseet
+
+data = sheet.get_all_records()  # Get a list of all records
+# pprint(data)
+row = sheet.row_values(3)  # Get a specific row
+# print(row)
+col = sheet.col_values(3)  # Get a specific column
+cell = sheet.cell(1,2).value  # Get the value of a specific cell
+
+# insertRow = ["hello", 5, "red", "blue"]
+# sheet.insert_row(row, 4)  # Insert the list as a row at index 4
+# sheet.delete_row(4)
+# sheet.update_cell(2,2, "CHANGED")  # Update one cell
+# 
+# numRows = sheet.row_count  # Get the number of rows in the sheet
